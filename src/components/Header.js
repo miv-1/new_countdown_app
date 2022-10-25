db2e4115b11195822f50331341b35722b33d0a00
Václav Míka <vaclav.mika14@gmail.com>
2022-10-25 19:15:13 +0200

    main page update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">Ivan's countdown</Navbar.Brand>
+            <Navbar.Brand href="/">My countdown</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
@@ -16,10 +16,6 @@ function Header() {
               <Nav className="mr-auto header;">
                 <Nav.Link href="/HelloThere">Hello there</Nav.Link>
               </Nav>
-              <Nav
-                className="justify-content-end"
-                style={{ width: "100%" }}
-              ></Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
